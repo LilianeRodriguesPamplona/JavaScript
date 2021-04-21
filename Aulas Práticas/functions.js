882d161844eaff4216acabcf394c21cc3811cbb0
Liliane Rodrigues <liliane.rodriguesssz@gmail.com>
2021-04-20 23:00:51 -0300

    JavaScript Aulas

---
+++ Aulas Práticas/functions.js
@@ -0,0 +1,203 @@
+
+/*Condições e operadores.
+
+window.onload = function(){
+	
+	// var nome = '';
+	// Principais operadores!
+	// === identico:tem que ser o mesmo valor e o mesmo tipo de variável.
+	// == apenas o valor precisa ser igual
+	// != diferente
+	// !== diferente valor e tipo
+	// >= maior igual
+	// <= menor igual
+	// > maior
+	// < menor
+
+	/*if(nome == 'joão'){
+		alert("condição verdadeira!");
+		}else if(nome == 'Liliane'){
+			alert("Condição verdadeira!");
+		}else{
+			alert("Condição falsa");*/
+
+		/*var idade = 2016-1993;
+
+			if(idade > 23){
+				console.log("Eu tenho mais de 23 anos de idade!");
+			}else{
+				console.log("Eu sou menor ou igual a 23");*/
+
+				/*var idade = 2016-1993;
+
+			if(idade > 23){
+				console.log("Eu tenho mais de 23 anos de idade!");
+			}else if(idade < 10){
+				console.log("Menor do que 10!");
+			}else if(idade < 5){
+				console.log("Menor do que 5!");
+			}else{
+				console.log("Nehuma das condições são verdadeiras!");*/
+
+				/*if(nome = 'Liliane'){
+					console.log(nome);*/
+
+					/*var number = '23';
+					if(number == 23){
+						console.log("Verdade");*/
+
+					/*var nome = 'Liliane';
+					if(nome != 'Liliane'){
+						console.log("Verdade");*/
+
+						/*var numero = 10;
+					if(numero !== '10'){
+						console.log("Verdade");
+	}
+}
+*/
+
+
+/* Laços de repetição.
+
+window.onload = function(){
+
+	/*var repeticao = 0;
+
+// while -> Precisamos de uma variável inicial, aqui se faz a verificação primeiro antes de executar a ação.
+	while(repeticao < 10){
+		console.log("Olá mundo número: "+repeticao);
+		repeticao++;
+	}
+
+	console.log("Cheguei até o final!");*/
+
+	/*var repeticao = 0;
+	var nome = 'Liliane';
+
+	while(nome == 'Liliane'){
+		console.log("Olá mundo");
+		nome = 'João';
+	}
+
+	console.log("Cheguei até o final!");*/
+
+	/*for(var numero = 0; numero < 10; numero++){
+		console.log("Olá mundo!");
+	}
+	*/
+
+	/*Ele executa primeiro o código e depois faz a verificação do while.
+	do{
+		console.log("Olá mundo");
+	}while(nome == 'Liliane');*./
+
+}
+*/
+
+
+/*Funções.*/
+/*window.onload = function(){
+
+	/*function realizarConta(){
+		console.log("Minha funcao");
+	}
+
+	realizarConta();
+
+}*/
+
+	/*function realizarConta(num1,num2){
+		console.log(num1+num2);
+	}
+
+	realizarConta(10,20);
+
+ }*/
+
+	/*function realizarConta(num1,num2){
+		if(num1 == 10){
+		console.log("Meu número 1 é igual a 10");
+	}
+
+}
+	realizarConta(10,20);
+}*/
+
+	/*var teste = 'Liliane';
+
+	function realizarConta(){
+		teste = 'João';
+		console.log(teste);
+	}
+
+	realizarConta();
+
+ }*/
+
+	/*function realizarConta(num1,num2){
+		return num1+num2;
+	}
+
+	var resultado = realizarConta(10,10);
+
+	console.log(resultado);
+
+ }*/
+
+ 	/*var variavel = function(){
+ 		console.log("Minha funcao através da variável");
+ 	};
+
+ 	console.log(typeof(variavel));
+
+ }*/
+
+ 	/*function teste(num1,num2){
+ 		return num1+num2;
+ 	}
+
+ 	var variavel = teste(10,20);
+
+ 	console.log(variavel);
+
+}*/
+
+	/*var obj = {'nome':'Liliane','idade':23,'func':function(){
+	console.log("Liliane");
+
+}};
+
+	obj.func();
+
+}*/
+
+	/*var obj = {'nome':'Liliane','idade':23};
+
+	obj.func = function(){
+		console.log('minha funcao');
+	}
+
+	obj.func();
+
+}*/
+
+	/*var variavel = [];
+
+	variavel[0] = [];
+
+	variavel[0][2] = 'Liliane';
+
+	console.log(variavel[0][2]);
+}*/
+	
+	/*var variavel = [];
+
+	variavel.obj = {};
+
+	variavel.obj.nome = 'Liliane';
+
+	console.log(variavel.obj.nome);
+
+}*/
+
